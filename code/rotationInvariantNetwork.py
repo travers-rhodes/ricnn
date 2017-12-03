42268bd0ec00b63120341605b97d3da4a45ccb57
Travers Rhodes <traversr@andrew.cmu.edu>
2017-12-03 12:22:32 -0500

    save variables and add tester

--- code/rotationInvariantNetwork.py
+++ code/rotationInvariantNetwork.py
@@ -39,23 +39,23 @@ class RICNN:
     # likewise for the bias terms of each layer
     self.layersBias = {}
     # the first layer is special for our indexing because it takes in one input and returns channelSizes[0] (we don't have channelSizes[-1]
-    self.layersWeights[0] = tf.Variable(tf.truncated_normal([filtRadius+1,1,channelSizes[0]], dtype = tf.float32, stddev = initializationConstant))
-    self.layersBias[0] = tf.Variable(tf.constant(initializationConstant, shape=[channelSizes[0]]))
+    self.layersWeights[0] = tf.Variable(tf.truncated_normal([filtRadius+1,1,channelSizes[0]], dtype = tf.float32, stddev = initializationConstant), name="convw%d"%0)
+    self.layersBias[0] = tf.Variable(tf.constant(initializationConstant, shape=[channelSizes[0]]), name="convb%d"%0)
     for layerId in range(1,self.numLayers):
       # the layer takes in something of with channelSizes[layerId-1] number of channels and returns something with channelSizes[layerId] number
       # of channels
       self.layersWeights[layerId] = tf.Variable(
-        tf.truncated_normal([filtRadius+1,channelSizes[layerId-1],channelSizes[layerId]], dtype = tf.float32, stddev = initializationConstant))
-      self.layersBias[layerId] = tf.Variable(tf.constant(initializationConstant, shape=[channelSizes[layerId]]))
+        tf.truncated_normal([filtRadius+1,channelSizes[layerId-1],channelSizes[layerId]], dtype = tf.float32, stddev = initializationConstant), name="convw%d"%layerId)
+      self.layersBias[layerId] = tf.Variable(tf.constant(initializationConstant, shape=[channelSizes[layerId]]), name="convb%d"%layerId)
   
     # add a fully connected layer
-    self.W_fc1 = tf.Variable(tf.truncated_normal([channelSizes[self.numLayers-1],fullyConnectedLayerWidth], stddev = initializationConstant))
+    self.W_fc1 = tf.Variable(tf.truncated_normal([channelSizes[self.numLayers-1],fullyConnectedLayerWidth], stddev = initializationConstant), name="fcw1")
     #print(self.W_fc1.shape)
-    self.b_fc1 = tf.Variable(tf.constant(initializationConstant,shape=[fullyConnectedLayerWidth]))
+    self.b_fc1 = tf.Variable(tf.constant(initializationConstant,shape=[fullyConnectedLayerWidth]), name="fcb1")
     
 
-    self.W_fc2 = tf.Variable(tf.truncated_normal([fullyConnectedLayerWidth, numOutputClasses], stddev = initializationConstant))
-    self.b_fc2 = tf.Variable(tf.constant(initializationConstant,shape=[numOutputClasses]))
+    self.W_fc2 = tf.Variable(tf.truncated_normal([fullyConnectedLayerWidth, numOutputClasses], stddev = initializationConstant), name="fcw2")
+    self.b_fc2 = tf.Variable(tf.constant(initializationConstant,shape=[numOutputClasses]), name="fcb2")
 
   # Set up our network by plugging in the consumer's placeholder nodes to our network, defining the nodes
   # in the network based on the weight values generated in __init__, and then returning the output node.

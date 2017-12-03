a451321415ef24c2e9f9b111fe7b1ead8ab35d9f
Samuel Clarke <sclarke@gatech.edu>
2017-12-03 00:08:17 -0500

    Attempt to tie network and main method together (just training).

--- code/main.py
+++ code/main.py
@@ -9,9 +9,7 @@ import tarfile
 import tensorflow as tf
 from IPython.display import display, Image
 from scipy import ndimage
-
-# Config the matplotlib backend as plotting inline in IPython
-%matplotlib inline
+from rotationInvariantNetwork import RICNN
 
 from tensorflow.examples.tutorials.mnist import input_data
 
@@ -26,12 +24,19 @@ tf.reset_default_graph()
 tf_train_dataset = tf.placeholder(
     tf.float32, shape=(batch_size, image_size * image_size))
 tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
-tf_valid_dataset = tf.constant(mnist.valid.images)
-valid_labels = mnist.valid.labels
+keep_prob = tf.placeholder(tf.float32)
+tf_valid_dataset = tf.constant(mnist.validation.images)
+valid_labels = mnist.validation.labels
 tf_test_dataset = tf.constant(mnist.test.images)
 test_labels = mnist.test.labels
 
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels))
+numOutputClasses = 10
+fullyConnectedLayerWidth = 1024
+nn = RICNN([4, 8, 16], fullyConnectedLayerWidth, numOutputClasses)
+
+logits = nn.setupNodes(tf_train_dataset, keep_prob)
+
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=tf_train_labels))
 
 optimizer = tf.train.AdamOptimizer(0.0005).minimize(loss)
 sess = tf.Session()
@@ -43,7 +48,8 @@ for i in range(30000):
     sess.run(optimizer, feed_dict = {X_in: batch_in, Y: batch_out, keep_prob: 0.8})
         
     if not i % 1000:
-        ls = sess.run([loss], feed_dict = {X_in: batch.images, Y: batch.labels, keep_prob: 1.0})
+        ls = sess.run([loss], feed_dict = {tf_train_dataset: batch.images, tf_train_labels: batch.labels, keep_prob: 1.0})
+        print(i, ls)
         """ls, d, i_ls, mu, sigm = sess.run([loss, dec, img_loss, mn, sd], feed_dict = {X_in: batch.images, Y: batch.labels, keep_prob: 1.0})
         plt.imshow(np.reshape(batch[0], [28, 28]), cmap='gray')
         plt.show()
